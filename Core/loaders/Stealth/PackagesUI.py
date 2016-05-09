e62a5bcf5cf5669454e86ab677eeb66d24d47a44
mh4x0f <mh4root@gmail.com>
2016-05-09 11:34:30 -0300

    fixed::set_monitor_mode Deauth attack module error #35

--- Core/loaders/Stealth/PackagesUI.py
+++ Core/loaders/Stealth/PackagesUI.py
@@ -1,6 +1,6 @@
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
-from Core.Utils import Refactor
+from Core.Utils import Refactor,set_monitor_mode
 from subprocess import Popen,PIPE
 from Core.utility.settings import frm_Settings
 from Modules.servers.PhishingManager import frm_PhishingManager
