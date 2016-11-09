f8a20575d150cdc028397d2578e90e943f847b36
Marcos Nesster <mh4root@gmail.com>
2016-11-09 09:54:07 -0300

    fixed Exception error issue #112

--- core/main.py
+++ core/main.py
@@ -1089,9 +1089,8 @@ class WifiPumpkin(QWidget):
             except Exception:
                 try:
                     check_output(['nmcli','nm','wifi',"off"]) # new version
-                except Exception,e:
-                    print '[!] ' + e
-                    return QMessageBox.warning(self,'Error nmcli',str(e))
+                except Exception as error:
+                    return QMessageBox.warning(self,'Error nmcli',str(error))
             finally:
                 call(['rfkill', 'unblock' ,'wifi'])
         elif self.interfacesLink['activated'][1] == 'wireless':
